2dc94c6f0c15c5b2f01c161d4557ed3602516bc5
Ariadna de los Angeles Coronel <ariadnacoronel127@gmail.com>
2024-01-14 18:36:25 -0300

    cambios css

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -3,7 +3,7 @@ import Item from '../Item/Item';
 import './ItemList.css'
 const ItemList = ({items}) => {
     return (
-        <div className='d-flex justify-content-center flex-wrap gap-5 mt-5 mb-5'>
+        <div className='contenedor-items'>
             {items.map((item)=>
             <div key={item.id} className='div-item'>
                 <Item item={item}/>
@@ -14,3 +14,4 @@ const ItemList = ({items}) => {
 }
 
 export default ItemList
+// className='d-flex justify-content-center flex-wrap gap-5 mt-5 mb-5'
